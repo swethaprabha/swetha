3e170092d8de45cc271027f2a563b99bf67b7588
swethaprabha <35984978+swethaprabha@users.noreply.github.com>
2018-12-01 12:51:59 +0530

    Add files via upload

---
+++ sum1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+		// your code goes here
+
+	int n;
+
+	scanf("%d",&n);
+	
+if(n>0)
+
+	printf("Positive\n");
+
+	else if(n<0)
+	
+printf("Negative\n");
+	
+else
+	
+printf("Zero\n");
+
+	
+	return 0;
+}
